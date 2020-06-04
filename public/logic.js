66ce25d5534d3d8240001cdae2d8b943c2a9663e
ZGS <zgstowell@gmail.com>
2020-06-04 11:57:52 -0700

    contact form working

--- public/logic.js
+++ public/logic.js
@@ -143,25 +143,27 @@ $(".hover-me").click(function () {
 // CONTACT ME FORM SETUP
 // =====================
 
-$("#contactme-form").on("submit", function (event) {
-    event.preventDefault();
-    const username = $("#sender-name").val().trim();
-    const userEmail = $("#sender-email").val().trim();
-    const userMessage = $("#sender-message").val().trim();
+// $("#contactme-form").on("submit", function (event) {
+//     event.preventDefault();
+//     console.log('clicked')
+//     const username = $("#sender-name").val().trim();
+//     const userEmail = $("#sender-email").val().trim();
+//     const userMessage = $("#sender-message").val().trim();
+//     console.log(username, userEmail, userMessage)
 
-    const messageObj = {
-        username: $("#sender-name").val().trim(),
-        userEmail: $("#sender-email").val().trim(),
-        userMessage: $("#sender-message").val().trim()
-    }
+//     const messageObj = {
+//         username: $("#sender-name").val().trim(),
+//         userEmail: $("#sender-email").val().trim(),
+//         userMessage: $("#sender-message").val().trim()
+//     }
 
-    $.ajax({
-        url: "/contactme",
-        method: "POST",
-        data: messageObj
-    }).then(function (res) {
-        console.log(status);
-        location.reload();
-    })
+//     $.ajax({
+//         url: "/contactme",
+//         method: "POST",
+//         data: messageObj
+//     }).then(function (res) {
+//         console.log(status);
+//         location.reload();
+//     })
 
-})
+// })
