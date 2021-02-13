cc3c584f0bb20912b95527c1da27fc8978841639
ZGS <zgstowell@gmail.com>
2021-02-12 23:42:09 -0800

    started working on slide-out side nav

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,17 @@
-console.log('linked')
+// LISTEN FOR CLICK TO OPEN SIDE NAV
+let menuState = false;
+$("body").on('click','.menu-btn', (e)=>{
+    console.log("clicked")
+
+    if (menuState===false){
+        $(".menu-open").hide(600)
+        $(".side-nav").css("left","0")
+        menuState = true;
+        console.log(menuState)
+    } else {
+        $(".menu-open").show(600)
+        $(".side-nav").css("left","-20vw")
+        menuState = false;
+        console.log(menuState)
+    }
+})
